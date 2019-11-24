c5611bcbae59f517df58f6fc7fb0f005a839f946
Snownee <1850986885@qq.com>
2019-11-24 20:23:28 +0800

    Fix #26

--- src/main/java/snownee/snow/block/SnowSlabBlock.java
+++ src/main/java/snownee/snow/block/SnowSlabBlock.java
@@ -36,42 +36,35 @@ import snownee.kiwi.util.Util;
 import snownee.snow.MainModule;
 import snownee.snow.SnowCommonConfig;
 
-public class SnowSlabBlock extends ModBlock implements ISnowVariant
-{
+public class SnowSlabBlock extends ModBlock implements ISnowVariant {
     protected static final VoxelShape BOTTOM_SHAPE = Block.makeCuboidShape(0.0D, 0.0D, 0.0D, 16.0D, 8.0D, 16.0D);
+    protected static final VoxelShape BOTTOM_RENDER_SHAPE = Block.makeCuboidShape(0.0D, 0.0D, 0.0D, 16.0D, 10.0D, 16.0D);
 
-    public SnowSlabBlock(Properties builder)
-    {
+    public SnowSlabBlock(Properties builder) {
         super(builder);
     }
 
     @Override
-    public boolean hasTileEntity(BlockState state)
-    {
+    public boolean hasTileEntity(BlockState state) {
         return true;
     }
 
     @Override
-    public TileEntity createTileEntity(BlockState state, IBlockReader world)
-    {
+    public TileEntity createTileEntity(BlockState state, IBlockReader world) {
         return new SnowTextureTile();
     }
 
     @Override
-    public boolean onBlockActivated(BlockState state, World worldIn, BlockPos pos, PlayerEntity player, Hand handIn, BlockRayTraceResult hit)
-    {
-        if (worldIn.isRemote)
-        {
+    public boolean onBlockActivated(BlockState state, World worldIn, BlockPos pos, PlayerEntity player, Hand handIn, BlockRayTraceResult hit) {
+        if (worldIn.isRemote) {
             return true;
         }
 
         TileEntity tile = worldIn.getTileEntity(pos);
         ItemStack stack = player.getHeldItem(handIn);
-        if (hit.getFace() == Direction.UP && tile instanceof TextureTile && ((TextureTile) tile).getMark("0") == stack.getItem() && stack.getItem() instanceof BlockItem && stack.getItem().isIn(ItemTags.SLABS))
-        {
+        if (hit.getFace() == Direction.UP && tile instanceof TextureTile && ((TextureTile) tile).getMark("0") == stack.getItem() && stack.getItem() instanceof BlockItem && stack.getItem().isIn(ItemTags.SLABS)) {
             Block block = ((BlockItem) stack.getItem()).getBlock();
-            if (block instanceof SlabBlock)
-            {
+            if (block instanceof SlabBlock) {
                 BlockState state2 = block.getDefaultState().with(SlabBlock.TYPE, SlabType.DOUBLE);
                 worldIn.setBlockState(pos, state2);
                 return true;
@@ -81,39 +74,37 @@ public class SnowSlabBlock extends ModBlock implements ISnowVariant
     }
 
     @Override
-    public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context)
-    {
+    public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
         return BOTTOM_SHAPE;
     }
 
     @Override
-    public boolean allowsMovement(BlockState state, IBlockReader worldIn, BlockPos pos, PathType type)
-    {
+    public VoxelShape getRenderShape(BlockState state, IBlockReader worldIn, BlockPos pos) {
+        return BOTTOM_RENDER_SHAPE;
+    }
+
+    @Override
+    public boolean allowsMovement(BlockState state, IBlockReader worldIn, BlockPos pos, PathType type) {
         return false;
     }
 
     @Override
     @OnlyIn(Dist.CLIENT)
-    public void addInformation(ItemStack stack, IBlockReader worldIn, List<ITextComponent> tooltip, ITooltipFlag flagIn)
-    {
+    public void addInformation(ItemStack stack, IBlockReader worldIn, List<ITextComponent> tooltip, ITooltipFlag flagIn) {
         String key = Util.getTextureItem(stack, "0");
-        if (!key.isEmpty())
-        {
+        if (!key.isEmpty()) {
             tooltip.add(new TranslationTextComponent(key).applyTextStyle(TextFormatting.GRAY));
         }
     }
 
     @Override
-    public void fillItemGroup(ItemGroup group, NonNullList<ItemStack> items)
-    {
+    public void fillItemGroup(ItemGroup group, NonNullList<ItemStack> items) {
         MainModule.fillTextureItems(ItemTags.SLABS, this, items);
     }
 
     @Override
-    public void randomTick(BlockState state, World worldIn, BlockPos pos, Random random)
-    {
-        if (!SnowCommonConfig.snowNeverMelt && worldIn.getLightFor(LightType.BLOCK, pos) > 11)
-        {
+    public void randomTick(BlockState state, World worldIn, BlockPos pos, Random random) {
+        if (!SnowCommonConfig.snowNeverMelt && worldIn.getLightFor(LightType.BLOCK, pos) > 11) {
             worldIn.setBlockState(pos, getRaw(state, worldIn, pos));
         }
     }
