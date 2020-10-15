edac600ab6205899cea2ab7b5390c893f66abea4
Snownee <1850986885@qq.com>
2020-10-16 02:26:04 +0800

    ...

--- src/main/java/snownee/snow/SnowRealMagic.java
+++ src/main/java/snownee/snow/SnowRealMagic.java
@@ -4,13 +4,12 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import net.minecraftforge.fml.common.Mod;
-import snownee.kiwi.Kiwi;
 
 @Mod(SnowRealMagic.MODID)
 public class SnowRealMagic {
     public static final String MODID = "snowrealmagic";
     public static final String NAME = "Snow! Real Magic!";
 
-    public static Logger logger = LogManager.getLogger(Kiwi.NAME);
+    public static Logger logger = LogManager.getLogger(SnowRealMagic.NAME);
     public static boolean mixin;
 }
