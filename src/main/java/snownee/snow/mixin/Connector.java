e1d798c34b42972955f8bf5b5e6739711039f7dd
Snownee <1850986885@qq.com>
2020-11-26 10:54:33 +0800

    .

--- src/main/java/snownee/snow/mixin/Connector.java
+++ src/main/java/snownee/snow/mixin/Connector.java
@@ -3,15 +3,13 @@ package snownee.snow.mixin;
 import org.spongepowered.asm.mixin.Mixins;
 import org.spongepowered.asm.mixin.connect.IMixinConnector;
 
-import snownee.snow.MixinConstants;
-
 public class Connector implements IMixinConnector {
 
     @Override
     public void connect() {
-        MixinConstants.logger.info("Invoking Mixin Connector");
+        //MixinConstants.logger.info("Invoking Mixin Connector");
         Mixins.addConfiguration("assets/snowrealmagic/snowrealmagic.mixins.json");
-        MixinConstants.mixin = true;
+        //MixinConstants.mixin = true;
     }
 
 }
