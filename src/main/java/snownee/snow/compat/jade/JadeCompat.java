9c3e3e73258c601c3f5958b61b6c4c3bdb9b4da8
Snownee <i@snownee.com>
2022-11-22 22:30:02 +0800

    feat: Jade compat

--- src/main/java/snownee/snow/compat/jade/JadeCompat.java
+++ src/main/java/snownee/snow/compat/jade/JadeCompat.java
@@ -1,23 +1,20 @@
-/*
 package snownee.snow.compat.jade;
 
 import static snownee.snow.CoreModule.*;
 
 import java.util.List;
 
-import mcp.mobius.waila.api.IRegistrar;
-import mcp.mobius.waila.api.IWailaPlugin;
-import mcp.mobius.waila.api.WailaPlugin;
-import net.minecraft.world.level.block.Block;
+import snownee.jade.api.IWailaClientRegistration;
+import snownee.jade.api.IWailaPlugin;
+import snownee.jade.api.WailaPlugin;
 
 @WailaPlugin
 public class JadeCompat implements IWailaPlugin {
 
 	@Override
-	public void register(IRegistrar registrar) {
-		for (Block block : List.of(TILE_BLOCK, FENCE, FENCE2, STAIRS, SLAB, FENCE_GATE, WALL))
-			registrar.usePickedResult(block);
+	public void registerClient(IWailaClientRegistration registration) {
+		for (var block : List.of(TILE_BLOCK, FENCE, FENCE2, STAIRS, SLAB, FENCE_GATE, WALL))
+			registration.usePickedResult(block.get());
 	}
 
 }
-*/
