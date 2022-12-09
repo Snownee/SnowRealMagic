d766c16dff464bda3521cbc40507c6fd9cf469ae
Snownee <i@snownee.com>
2022-12-10 02:23:50 +0800

    feat: implement block appearance api

--- src/main/java/snownee/snow/block/SnowVariant.java
+++ src/main/java/snownee/snow/block/SnowVariant.java
@@ -1,9 +1,14 @@
 package snownee.snow.block;
 
+import org.jetbrains.annotations.Nullable;
+
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.tags.BlockTags;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.BlockAndTintGetter;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelReader;
@@ -12,6 +17,7 @@ import net.minecraft.world.level.block.SnowLayerBlock;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.phys.HitResult;
 import net.minecraftforge.common.extensions.IForgeBlock;
 import snownee.snow.WrappedSoundType;
@@ -47,6 +53,18 @@ public interface SnowVariant extends IForgeBlock {
 			return WrappedSoundType.get(getRaw(state, world, pos).getSoundType(world, pos, entity));
 		}
 		return IForgeBlock.super.getSoundType(state, world, pos, entity);
+	}
+
+	@Override
+	default BlockState getAppearance(BlockState state, BlockAndTintGetter level, BlockPos pos, Direction side, @Nullable BlockState queryState, @Nullable BlockPos queryPos) {
+		if (queryState != null && queryState.is(BlockTags.SNOW)) {
+			BlockState appearance = Blocks.SNOW.defaultBlockState();
+			if (state.hasProperty(BlockStateProperties.LAYERS)) {
+				appearance = appearance.setValue(BlockStateProperties.LAYERS, state.getValue(BlockStateProperties.LAYERS));
+			}
+			return appearance;
+		}
+		return getRaw(state, level, pos);
 	}
 
 }
