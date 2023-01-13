e0d4ce9d0d56455a7699d1e6fd509ec52cc08c99
Snownee <i@snownee.com>
2023-01-14 04:43:50 +0800

    feat: implement block appearance api

--- src/main/java/snownee/snow/block/SnowVariant.java
+++ src/main/java/snownee/snow/block/SnowVariant.java
@@ -3,19 +3,24 @@ package snownee.snow.block;
 import org.jetbrains.annotations.Nullable;
 
 import net.fabricmc.fabric.api.block.BlockPickInteractionAware;
+import net.fabricmc.fabric.api.block.v1.FabricBlock;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.tags.BlockTags;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.BlockAndTintGetter;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.phys.HitResult;
 import snownee.kiwi.block.IKiwiBlock;
 import snownee.snow.block.entity.SnowBlockEntity;
 
-public interface SnowVariant extends IKiwiBlock {
+public interface SnowVariant extends IKiwiBlock, FabricBlock {
 	default BlockState getRaw(BlockState state, BlockGetter world, BlockPos pos) {
 		if (state.hasBlockEntity()) {
 			BlockEntity tile = world.getBlockEntity(pos);
@@ -50,5 +55,17 @@ public interface SnowVariant extends IKiwiBlock {
 	//		}
 	//		return IForgeBlock.super.getSoundType(state, world, pos, entity);
 	//	}
+
+	@Override
+	default BlockState getAppearance(BlockState state, BlockAndTintGetter level, BlockPos pos, Direction side, @Nullable BlockState queryState, @Nullable BlockPos sourcePos) {
+		if (queryState != null && queryState.is(BlockTags.SNOW)) {
+			BlockState appearance = Blocks.SNOW.defaultBlockState();
+			if (state.hasProperty(BlockStateProperties.LAYERS)) {
+				appearance = appearance.setValue(BlockStateProperties.LAYERS, state.getValue(BlockStateProperties.LAYERS));
+			}
+			return appearance;
+		}
+		return getRaw(state, level, pos);
+	}
 
 }
