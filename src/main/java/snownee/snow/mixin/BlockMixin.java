6ebf33b005c0422e0c9e31302bb82f6f26e440bb
Snownee <i@snownee.com>
2023-01-14 12:35:06 +0800

    remove unused hook

--- src/main/java/snownee/snow/mixin/BlockMixin.java
+++ src/main/java/snownee/snow/mixin/BlockMixin.java
@@ -5,25 +5,12 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import net.minecraft.core.BlockPos;
-import net.minecraft.core.Direction;
-import net.minecraft.world.InteractionResult;
-import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.SnowLayerBlock;
 import net.minecraft.world.level.block.state.BlockState;
-import snownee.snow.Hooks;
 
 @Mixin(Block.class)
 public class BlockMixin {
-
-	@Inject(at = @At("HEAD"), method = "shouldRenderFace", cancellable = true)
-	private static void srm_shouldRenderFace(BlockState state, BlockGetter level, BlockPos pos, Direction direction, BlockPos relativePos, CallbackInfoReturnable<Boolean> ci) {
-		InteractionResult result = Hooks.shouldRenderFaceSnow(state, level, pos, direction, relativePos);
-		if (result.consumesAction()) {
-			ci.setReturnValue(result == InteractionResult.SUCCESS);
-		}
-	}
 
 	@Inject(at = @At("HEAD"), method = "isExceptionForConnection", cancellable = true)
 	private static void srm_isExceptionForConnection(BlockState state, CallbackInfoReturnable<Boolean> ci) {
