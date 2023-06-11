82e03bf9c3c8c22964c92870d996c8ea88c447d8
Snownee <i@snownee.com>
2023-06-11 10:26:53 +0800

    fix: #288

---
+++ src/main/java/snownee/snow/mixin/MixinPlugin.java
@@ -0,0 +1,52 @@
+package snownee.snow.mixin;
+
+import java.util.List;
+import java.util.Set;
+
+import org.objectweb.asm.tree.ClassNode;
+import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
+import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
+
+import net.minecraftforge.fml.loading.LoadingModList;
+
+public class MixinPlugin implements IMixinConfigPlugin {
+
+	private boolean hasKiwi;
+
+	private static boolean hasMod(String modid) {
+		return LoadingModList.get().getModFileById(modid) != null;
+	}
+
+	@Override
+	public void onLoad(String mixinPackage) {
+		hasKiwi = hasMod("kiwi");
+	}
+
+	@Override
+	public String getRefMapperConfig() {
+		return null;
+	}
+
+	@Override
+	public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
+		return hasKiwi;
+	}
+
+	@Override
+	public void acceptTargets(Set<String> myTargets, Set<String> otherTargets) {
+	}
+
+	@Override
+	public List<String> getMixins() {
+		return null;
+	}
+
+	@Override
+	public void preApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+	}
+
+	@Override
+	public void postApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+	}
+
+}
