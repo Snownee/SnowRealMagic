4156ec3a531074d058e0663aa381937f0c94a9a1
Snownee <i@snownee.com>
2023-07-03 04:40:27 +0800

    Revert "fix: #290"
    This reverts commit 33796094e6fce532b5b814633c32b4d9f9533ad8.

--- src/main/java/snownee/snow/block/EntitySnowLayerBlock.java
+++ src/main/java/snownee/snow/block/EntitySnowLayerBlock.java
@@ -189,7 +189,9 @@ public class EntitySnowLayerBlock extends SnowLayerBlock implements EntityBlock,
 		InteractionResult result = getRaw(state, worldIn, pos).use(worldIn, player, handIn, hit);
 		if (result.consumesAction()) {
 			BlockState stateNow = worldIn.getBlockState(pos);
-			Hooks.convert(worldIn, pos, stateNow, state.getValue(LAYERS), 18, true);
+			if (!stateNow.is(this)) {
+				Hooks.convert(worldIn, pos, stateNow, state.getValue(LAYERS), 18, true);
+			}
 			return result;
 		}
 		return super.use(state, worldIn, pos, player, handIn, hit);
