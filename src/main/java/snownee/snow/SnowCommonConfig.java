b534d8fe60231701b15a704c6f183b49861f7222
Snownee <i@snownee.com>
2025-03-29 22:46:39 +0800

    improve: clear cache when options refreshed

--- src/main/java/snownee/snow/SnowCommonConfig.java
+++ src/main/java/snownee/snow/SnowCommonConfig.java
@@ -1,76 +1,85 @@
 package snownee.snow;
 
-import snownee.kiwi.KiwiModule.Skip;
+import net.minecraft.client.Minecraft;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
+import snownee.kiwi.KiwiModule;
 import snownee.kiwi.config.ConfigUI;
 import snownee.kiwi.config.KiwiConfig;
+import snownee.kiwi.config.KiwiConfig.Listen;
 import snownee.kiwi.config.KiwiConfig.Path;
+import snownee.kiwi.config.KiwiConfig.PerformanceImpact;
+import snownee.kiwi.config.KiwiConfig.PerformanceType;
 import snownee.kiwi.config.KiwiConfig.Range;
+import snownee.kiwi.loader.Platform;
+import snownee.snow.block.ShapeCaches;
+import snownee.snow.util.CommonProxy;
 
 @KiwiConfig
 public final class SnowCommonConfig {
 
-	@KiwiConfig.PerformanceImpact(KiwiConfig.PerformanceType.LOW)
+	@PerformanceImpact(PerformanceType.LOW)
 	public static boolean snowGravity = true;
-	@KiwiConfig.PerformanceImpact(KiwiConfig.PerformanceType.NONE)
+	@PerformanceImpact(PerformanceType.NONE)
 	public static boolean snowMakingIce = true;
-	@KiwiConfig.PerformanceImpact(KiwiConfig.PerformanceType.NONE)
+	@PerformanceImpact(PerformanceType.NONE)
 	public static boolean snowAlwaysReplaceable = true;
-	@KiwiConfig.PerformanceImpact(KiwiConfig.PerformanceType.NONE)
+	@PerformanceImpact(PerformanceType.NONE)
 	public static boolean thinnerBoundingBox = true;
-	@KiwiConfig.PerformanceImpact(KiwiConfig.PerformanceType.NONE)
+	@PerformanceImpact(PerformanceType.NONE)
 	public static boolean snowNeverMelt = false;
-	@KiwiConfig.PerformanceImpact(KiwiConfig.PerformanceType.NONE)
+	@PerformanceImpact(PerformanceType.NONE)
 	public static int snowSpawnMaxLightLevel = 9;
-	@KiwiConfig.PerformanceImpact(KiwiConfig.PerformanceType.NONE)
+	@PerformanceImpact(PerformanceType.NONE)
 	public static int snowPersistMaxLightLevel = 11;
-	@KiwiConfig.PerformanceImpact(KiwiConfig.PerformanceType.NONE)
+	@PerformanceImpact(PerformanceType.NONE)
 	public static boolean snowReduceFallDamage = true;
-	@KiwiConfig.PerformanceImpact(KiwiConfig.PerformanceType.NONE)
+	@PerformanceImpact(PerformanceType.NONE)
 	public static boolean sustainGrassIfLayerMoreThanOne = true;
-	@KiwiConfig.PerformanceImpact(KiwiConfig.PerformanceType.NONE)
+	@PerformanceImpact(PerformanceType.NONE)
 	public static boolean sneakSnowball = true;
-	@KiwiConfig.PerformanceImpact(KiwiConfig.PerformanceType.LOW)
+	@PerformanceImpact(PerformanceType.LOW)
 	public static boolean fancySnowOnUpperSlab = true;
 	@Range(min = 1, max = 8)
 	@ConfigUI.Slider
-	@KiwiConfig.PerformanceImpact(KiwiConfig.PerformanceType.NONE)
+	@PerformanceImpact(PerformanceType.NONE)
 	public static int mobSpawningMaxLayers = 8;
 	@Path("snow-cover.placeSnowOnBlock")
-	@KiwiConfig.PerformanceImpact(KiwiConfig.PerformanceType.LOW)
+	@PerformanceImpact(PerformanceType.LOW)
 	public static boolean placeSnowOnBlock = true;
 	@Path("snow-cover.placeNaturally")
-	@KiwiConfig.PerformanceImpact(KiwiConfig.PerformanceType.LOW)
+	@PerformanceImpact(PerformanceType.LOW)
 	public static boolean placeSnowOnBlockNaturally = true;
 	@Path("snow-cover.replaceWorldgenFeature")
-	@KiwiConfig.PerformanceImpact(KiwiConfig.PerformanceType.LOW)
+	@PerformanceImpact(PerformanceType.LOW)
 	public static boolean replaceWorldFeature = true;
 	@Path("snow-cover.restoreOriginalBlocks")
-	@KiwiConfig.PerformanceImpact(KiwiConfig.PerformanceType.LOW)
+	@PerformanceImpact(PerformanceType.LOW)
 	public static boolean restoreOriginalBlocks = false;
 	@Path("accumulation.accumulatesDuringSnowfall")
-	@KiwiConfig.PerformanceImpact(KiwiConfig.PerformanceType.MEDIUM)
+	@PerformanceImpact(PerformanceType.MEDIUM)
 	public static boolean snowAccumulationDuringSnowfall = false;
 	@Path("accumulation.accumulatesDuringSnowstorm")
-	@KiwiConfig.PerformanceImpact(KiwiConfig.PerformanceType.MEDIUM)
+	@PerformanceImpact(PerformanceType.MEDIUM)
 	public static boolean snowAccumulationDuringSnowstorm = true;
 	@Path("accumulation.maxLayers")
 	@Range(min = 0, max = 9)
 	@ConfigUI.Slider
-	@KiwiConfig.PerformanceImpact(KiwiConfig.PerformanceType.LOW)
+	@PerformanceImpact(PerformanceType.LOW)
 	public static int snowAccumulationMaxLayers = 6;
 	@Path("accumulation.snowAndIceMeltInWarmBiomes")
-	@KiwiConfig.PerformanceImpact(KiwiConfig.PerformanceType.LOW)
+	@PerformanceImpact(PerformanceType.LOW)
 	public static boolean snowAndIceMeltInWarmBiomes = false;
 	@Path("accumulation.naturalMelting")
-	@KiwiConfig.PerformanceImpact(KiwiConfig.PerformanceType.MEDIUM)
+	@PerformanceImpact(PerformanceType.MEDIUM)
 	public static boolean snowNaturalMelt = true;
 	@Path("accumulation.smoothAccumulation")
-	@KiwiConfig.PerformanceImpact(KiwiConfig.PerformanceType.LOW)
+	@PerformanceImpact(PerformanceType.LOW)
 	public static boolean smoothAccumulation = true;
 
-	@Skip // Unavailable on Fabric
+	@KiwiModule.Skip // Unavailable on Fabric
 	@Path("integration.accumulationWinterOnly")
-	@KiwiConfig.PerformanceImpact(KiwiConfig.PerformanceType.NONE)
+	@PerformanceImpact(PerformanceType.NONE)
 	public static boolean accumulationWinterOnly = false;
 
 	@Path("debug.forceVanillaIceSnowLogic")
@@ -81,5 +90,17 @@ public final class SnowCommonConfig {
 	@SuppressWarnings("BooleanMethodIsAlwaysInverted")
 	public static boolean canPlaceSnowInBlock() {
 		return placeSnowOnBlock && !restoreOriginalBlocks;
+	}
+
+	@Listen("thinnerBoundingBox")
+	public static void onThinnerBoundingBoxChange(String path) {
+		ShapeCaches.invalidateAll();
+		if (Platform.isPhysicalClient() && Minecraft.getInstance().level != null) {
+			for (Block block : CommonProxy.allSnowBlocks()) {
+				for (BlockState blockState : block.getStateDefinition().getPossibleStates()) {
+					blockState.initCache();
+				}
+			}
+		}
 	}
 }
