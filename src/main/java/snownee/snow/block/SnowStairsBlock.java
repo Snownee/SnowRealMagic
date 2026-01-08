60f95c009bd294026c72e34f0d44b5e733af1bbc
SettingDust <settingdust@gmail.com>
2026-01-08 19:36:57 +0800

    fix: handle the block light for stair and slab
    relate #457
    
    # Conflicts:
    #	src/main/java/snownee/snow/block/SnowSlabBlock.java
    #	src/main/java/snownee/snow/block/SnowStairsBlock.java
    #	src/main/java/snownee/snow/util/CommonProxy.java

--- src/main/java/snownee/snow/block/SnowStairsBlock.java
+++ src/main/java/snownee/snow/block/SnowStairsBlock.java
@@ -52,7 +52,7 @@ public class SnowStairsBlock extends StairBlock implements WaterLoggableSnowVari
 
 	@Override
 	public void randomTick(BlockState state, ServerLevel worldIn, BlockPos pos, RandomSource random) {
-		if (SnowCommonConfig.retainOriginalBlocks || CommonProxy.shouldMelt(worldIn, pos)) {
+		if (SnowCommonConfig.retainOriginalBlocks || CommonProxy.shouldMeltInGeneral(worldIn, pos)) {
 			worldIn.setBlockAndUpdate(pos, getRaw(state, worldIn, pos));
 		}
 	}
