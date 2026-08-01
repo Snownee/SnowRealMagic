71220d0298839147659475abf12bc9c065dcb5a3
SettingDust <settingdust@gmail.com>
2026-08-01 20:14:02 +0800

    fix(neoforge): update platform and Kiwi registration

--- src/main/java/snownee/snow/CoreModule.java
+++ src/main/java/snownee/snow/CoreModule.java
@@ -26,6 +26,7 @@ import snownee.kiwi.KiwiModule;
 import snownee.kiwi.KiwiModule.Name;
 import snownee.kiwi.KiwiModule.NoItem;
 import snownee.kiwi.loader.event.InitEvent;
+import snownee.kiwi.block.entity.InheritanceBlockEntityType;
 import snownee.snow.block.ExtraCollisionSnowLayerBlock;
 import snownee.snow.block.SRMSnowLayerBlock;
 import snownee.snow.block.SnowFenceBlock;
@@ -117,9 +118,9 @@ public class CoreModule extends AbstractModule {
 	@Name("snow")
 	public static final KiwiGO<BlockEntityType<SnowBlockEntity>> TILE = blockEntity(SnowBlockEntity::new, SRMSnowLayerBlock.class);
 
-	public static final KiwiGO<BlockEntityType<SnowCoveredBlockEntity>> TEXTURE_TILE = blockEntity(
-			SnowCoveredBlockEntity::new,
-			WaterLoggableSnowVariant.class);
+	public static final KiwiGO<BlockEntityType<SnowCoveredBlockEntity>> TEXTURE_TILE = go(
+			() -> new InheritanceBlockEntityType<>(SnowCoveredBlockEntity::new, WaterLoggableSnowVariant.class, false),
+			Registries.BLOCK_ENTITY_TYPE);
 
 	public static final KiwiGO<MapCodec<NormalizeLoot>> NORMALIZE = go(() -> NormalizeLoot.CODEC, Registries.LOOT_POOL_ENTRY_TYPE);
 
